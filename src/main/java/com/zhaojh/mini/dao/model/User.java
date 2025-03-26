bb48cfada76572244da9b1e7181c50d9b4c45aac
JH Zhao <zhaojh@JHs-MacBook-Pro.local>
2025-03-26 15:13:13 -0700

    Added missing fields

--- src/main/java/com/zhaojh/mini/dao/model/User.java
+++ src/main/java/com/zhaojh/mini/dao/model/User.java
@@ -1,5 +1,6 @@
 package com.zhaojh.mini.dao.model;
 
+import com.zhaojh.mini.common.pojo.Role;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Data;
@@ -26,6 +27,7 @@ public class User {
     String passwordHash;
     String avatarUrl;
     String bio;
+    Role role;
 
     @CreatedBy
     String createdBy;
