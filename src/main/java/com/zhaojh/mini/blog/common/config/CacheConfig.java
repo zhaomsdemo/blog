f224a3304592b2414fc37da914e8552e97ad6df9
JH Zhao <zhaojh@JHs-MacBook-Pro.local>
2025-06-23 21:07:52 -0700

    Added Redis as cache service

--- src/main/java/com/zhaojh/mini/blog/common/config/CacheConfig.java
+++ src/main/java/com/zhaojh/mini/blog/common/config/CacheConfig.java
@@ -1,14 +1,53 @@
 package com.zhaojh.mini.blog.common.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.time.Duration;
 
 /**
  * Configuration class to enable caching in the application.
+ * Uses Redis as the cache service.
  */
 @Configuration
 @EnableCaching
 public class CacheConfig {
-    // The @EnableCaching annotation triggers the Spring Framework's caching infrastructure
-    // Using default SimpleCache provider as no specific cache implementation is configured
+
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
+
+    @Value("${spring.cache.redis.time-to-live}")
+    private long timeToLive;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration(redisHost, redisPort);
+        return new LettuceConnectionFactory(configuration);
+    }
+
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        RedisCacheConfiguration cacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMillis(timeToLive))
+                .disableCachingNullValues()
+                .serializeValuesWith(
+                        RedisSerializationContext.SerializationPair.fromSerializer(
+                                new GenericJackson2JsonRedisSerializer()));
+
+        return RedisCacheManager.builder(redisConnectionFactory)
+                .cacheDefaults(cacheConfiguration)
+                .build();
+    }
 }
--- src/main/java/com/zhaojh/mini/blog/common/vo/UserVo.java
+++ src/main/java/com/zhaojh/mini/blog/common/vo/UserVo.java
@@ -3,14 +3,18 @@ package com.zhaojh.mini.blog.common.vo;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.zhaojh.mini.blog.common.pojo.Role;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 
 @Data
 @Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserVo {
 
     String id;
